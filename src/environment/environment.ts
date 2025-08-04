74e76959a2ea111dbbc7aad9415c9f19234efdd9
Abhinav <agundupogula@ctepl.com>
2025-08-04 12:28:27 +0530

    url chnages

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 //   production: false,
   // apiBaseUrl: 'http://localhost:5174/api/Resource/',
-    apiBaseUrl: 'https://resource-tracker-app-ewb2cfggh2f5d4h2.canadacentral-01.azurewebsites.net/',
+    apiBaseUrl: 'https://resource-tracker-app-ewb2cfggh2f5d4h2.canadacentral-01.azurewebsites.net/api/Resource/',
 };
